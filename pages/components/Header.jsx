9b78739b4198106440e22a792be993ac691d4e93
benyamynbrkyc <benjamin.brkic.a@gmail.com>
2021-09-24 02:22:51 +0200

    add chat text

--- pages/components/Header.jsx
+++ pages/components/Header.jsx
@@ -6,16 +6,20 @@ function Main() {
 
   setInterval(() => {
     setAnimateObject({ y: -10 });
-  }, 1700);
+  }, 1300);
 
   return (
     <motion.main
       animate={{ scale: [0.75, 1], y: -30, rotate: [-15, 0] }}
-      transition={{ duration: 0.6, type: 'spring', damping: 4 }}
-      className='sticky max-w-sm text-lg text-center md:top-12 top-6 md:text-xl'>
+      transition={{ duration: 1, type: 'spring', damping: 4 }}
+      className='sticky max-w-sm text-base text-left md:top-12 top-6 md:text-xl'>
       <br />
-      <div className='px-6 py-4 text-white bg-blue-600 rounded-2xl'>
-        <p>Hello 👋, my name is Benjamin!</p>
+      <div className='px-6 py-4 text-white bg-blue-600 shadow-lg rounded-2xl'>
+        <p>
+          Hello, my name is Benjamin! 👋
+          <br />
+          I'm a Full Stack Developer. <br /> Nice to meet you 😄
+        </p>
       </div>
       <motion.img
         animate={animateObject}
--- pages/components/Main.jsx
+++ pages/components/Main.jsx
@@ -3,7 +3,7 @@ import 'animate.css';
 function Main() {
   return (
     <div className='animate__animated animate__fadeInUp animate__delay-1s'>
-      <h1>Main content</h1>
+      <h1>Coming soon...</h1>
     </div>
   );
 }
