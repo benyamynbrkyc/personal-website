2442b1b8e6e69c94ab938996b0b1f4c358b77464
Benjamin Brkic <benjamin.brkic.a@gmail.com>
2022-02-27 20:58:25 +0100

    feat(content): create coming soon message

--- pages/components/Main/index.jsx
+++ pages/components/Main/index.jsx
@@ -17,6 +17,14 @@ function Main() {
       <Spacer size='sm' />
 
       <ContactButton type='github'></ContactButton>
+
+      <Spacer />
+      <Spacer />
+
+      <p className='flex items-center gap-2 dark:text-white'>
+        <img src='/images/icons/hammer-wrench.webp' height={20} width={20} />
+        More coming soon...
+      </p>
     </div>
   );
 }
