6f099559847d808d356ec480014a8f360ed762c3
Benjamin Brkic <benjamin.brkic.a@gmail.com>
2022-03-07 01:55:57 +0100

    feat(page): create work page

---
+++ src/pages/work.tsx
@@ -0,0 +1,10 @@
+export default function work() {
+  return (
+    <div className='text-white'>
+      <p className='flex items-center justify-center gap-2 dark:text-white'>
+        <img src='/images/icons/hammer-wrench.webp' height={20} width={20} />
+        Coming soon...
+      </p>
+    </div>
+  );
+}
