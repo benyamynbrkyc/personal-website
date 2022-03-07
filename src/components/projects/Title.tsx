00a788776856d99c4fe9a9ea2725f4af88022d5b
Benjamin Brkic <benjamin.brkic.a@gmail.com>
2022-03-07 02:01:19 +0100

    chore: remove spacer

--- src/components/projects/Title.tsx
+++ src/components/projects/Title.tsx
@@ -1,12 +1,12 @@
-import Spacer from 'components/ui/Spacer';
+// import Spacer from 'components/ui/Spacer';
 
 export default function Title({ children }: { children: JSX.Element }) {
   return (
     <>
-      <h1 className='text-3xl border-b border-gray-200 dark:border-gray-800 title'>
+      <h1 className='title border-b border-gray-200 text-3xl dark:border-gray-800'>
         {children}
       </h1>
-      <Spacer size='sm' />
+      {/* <Spacer size='sm' /> */}
     </>
   );
 }
