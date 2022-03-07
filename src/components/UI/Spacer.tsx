6e1126f006cdc7a181fefce14c18dc6625573634
Benjamin Brkic <benjamin.brkic.a@gmail.com>
2022-03-07 01:59:43 +0100

    style(component): refactor white-space

--- src/components/UI/Spacer.tsx
+++ src/components/UI/Spacer.tsx
@@ -1,19 +1,17 @@
 export default function Spacer({ size = 'md' }) {
-  let spacer = <div className='w-full h-8'></div>;
+  let spacer = <div className='h-8 w-full'></div>;
   switch (size) {
     case 'md':
-      spacer = <div className='w-full h-8'></div>;
+      spacer = <div className='h-8 w-full'></div>;
       break;
-
     case 'sm':
-      spacer = <div className='w-full h-4'></div>;
+      spacer = <div className='h-4 w-full'></div>;
       break;
     case 'lg':
-      spacer = <div className='w-full h-12'></div>;
+      spacer = <div className='h-12 w-full'></div>;
       break;
-
     default:
-      spacer = <div className='w-full h-8'></div>;
+      spacer = <div className='h-8 w-full'></div>;
       break;
   }
   return spacer;
