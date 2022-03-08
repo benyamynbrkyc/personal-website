e666426647aa19d0e6556b46bde5eae1e80b726a
Benjamin Brkic <benjamin.brkic.a@gmail.com>
2022-03-08 02:06:16 +0100

    fix(layout): implement layout

---
+++ src/views/Index/index.tsx
@@ -0,0 +1,17 @@
+import Head from 'components/meta/Head';
+import Chat from 'views/Index/Chat';
+import ContactSection from 'views/Index/ContactSection';
+
+export default function Home() {
+  return (
+    <>
+      <Head />
+      <Chat />
+      <ContactSection />
+      <p className='mt-20 flex items-center justify-center gap-2 dark:text-white'>
+        <img src='/images/icons/hammer-wrench.webp' height={20} width={20} />
+        More coming soon...
+      </p>
+    </>
+  );
+}
