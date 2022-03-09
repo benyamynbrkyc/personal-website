b85af67c95186993750273d0f68bf7070b6213b7
Benjamin Brkic <benjamin.brkic.a@gmail.com>
2022-03-09 01:54:05 +0100

    refactor(work card): move work card to own folder

--- src/views/Work/WorkCard/WorkCard.tsx
+++ src/views/Work/WorkCard/WorkCard.tsx
@@ -1,16 +1,26 @@
 import Image from 'next/image';
 
-import type { Tag } from 'views/Work/WorkTags';
-import WorkTags from 'views/Work/WorkTags';
+import type { Tag } from 'views/Work/WorkCard/WorkCardTags';
+import WorkTags from 'views/Work/WorkCard/WorkCardTags';
+import WorkCardLinks from './WorkCardLinks';
 
 interface IProps {
   title: string;
   description: string;
   imgUrl: string;
   tags: Tag[];
+  liveUrl?: string;
+  githubUrl?: string;
 }
 
-export default function WorkCard({ title, description, imgUrl, tags }: IProps) {
+export default function WorkCard({
+  title,
+  description,
+  imgUrl,
+  tags,
+  liveUrl,
+  githubUrl,
+}: IProps) {
   return (
     <article className='flex w-full max-w-2xl flex-col gap-2 rounded-2xl border p-6 text-black dark:border-transparent dark:bg-gray-900 dark:text-white'>
       <div className='relative mb-2 rounded-2xl'>
@@ -25,6 +35,7 @@ export default function WorkCard({ title, description, imgUrl, tags }: IProps) {
       <h1 className='text-2xl font-bold'>{title}</h1>
       <p>{description}</p>
       <WorkTags tags={tags} />
+      <WorkCardLinks liveUrl={liveUrl} githubUrl={githubUrl} />
     </article>
   );
 }
