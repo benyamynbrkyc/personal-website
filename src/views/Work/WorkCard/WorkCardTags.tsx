4eec23160ce8e6e815999f2a3420497b58b49391
brkic.benjamin <brkic.benjamin@nsoft.com>
2023-09-06 14:00:55 +0200

    do not wrap text in tag

--- src/views/Work/WorkCard/WorkCardTags.tsx
+++ src/views/Work/WorkCard/WorkCardTags.tsx
@@ -10,7 +10,7 @@ export default function WorkTags({ tags }: { tags: Tag[] }) {
         <i
           style={{ backgroundColor: `${tag.color}32` || '#f3f4f632' }} // tailwind gray-100
           key={tag.name}
-          className='rounded-full py-1 px-2 text-center text-sm font-light italic'>
+          className='rounded-full py-1 px-2 text-center whitespace-nowrap text-sm font-light italic'>
           {tag.name}
         </i>
       ))}
