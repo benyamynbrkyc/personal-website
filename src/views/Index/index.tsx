77f9e9367a9d86dbb11e78e666784b57e24fe90f
brkic.benjamin <brkic.benjamin@nsoft.com>
2023-09-17 16:44:37 +0200

    remove coming soon

--- src/views/Index/index.tsx
+++ src/views/Index/index.tsx
@@ -10,16 +10,6 @@ export default function Home() {
       <Head />
       <ChatBubble />
       <ContactSection />
-      <p className='mt-20 flex items-center justify-center gap-2 dark:text-white'>
-        <Image
-          src='/images/icons/hammer-wrench.webp'
-          height={20}
-          width={20}
-          alt='Tool Emoji'
-          priority={true}
-        />
-        More coming soon...
-      </p>
     </>
   );
 }
